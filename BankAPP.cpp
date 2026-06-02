a1f2369386c19738bc91a3e79ff6219a9c51d23d
Janmesh311 <bjanmesh21@yahoo.com>
2026-06-01 22:17:42 -0400

    added savings and checking account classes

--- BankAPP.cpp
+++ BankAPP.cpp
@@ -67,6 +67,64 @@ class Account{
 
 };
 
+class SavingsAccount : public Account {
+    private:
+    double interestRate;
+
+    public:
+    SavingsAccount(string accNum, Customer* holder, double initialBalance, double rate) : Account(accNum, holder, initialBalance) {
+        interestRate = rate;
+    }
+
+    void withdraw(double amount) override {
+        if (balance - amount < 100) {   // cant go below $100
+            cout << "Denied. Savings account must keep $100 minimum balance." << endl;
+        } else {
+            balance -= amount;
+            cout << "Withdrew $" << amount << " | New Balance: $" << balance << endl;
+        }
+    }
+
+    void printReceipt() override {
+        cout << "SAVINGS ACCOUNT" << endl;
+        cout << "Account: " << accountNumber << endl;
+        cout << "Holder: " << getHolderName() << endl;
+        cout << "Balance: $" << balance << endl;
+        cout << "Interest Rate: " << interestRate << "%" << endl;
+    }
+
+
+};
+
+// Checking Account - can go negative up to overdraft limit
+class CheckingAccount : public Account {
+private:
+    double overdraftLimit;
+
+public:
+    CheckingAccount(string accNum, Customer* holder, double initialBalance, double limit)
+        : Account(accNum, holder, initialBalance) {
+        overdraftLimit = limit;
+    }
+
+    void withdraw(double amount) override {
+        if (balance - amount < -overdraftLimit) {   // cant exceed overdraft limit
+            cout << "Denied. Exceeds overdraft limit of $" << overdraftLimit << endl;
+        } else {
+            balance -= amount;
+            cout << "Withdrew $" << amount << " | New Balance: $" << balance << endl;
+        }
+    }
+
+    void printReceipt() override {
+        cout << "=== CHECKING ACCOUNT ===" << endl;
+        cout << "Account: " << accountNumber << endl;
+        cout << "Holder: " << getHolderName() << endl;
+        cout << "Balance: $" << balance << endl;
+        cout << "Overdraft Limit: $" << overdraftLimit << endl;
+    }
+};
+
 
 void welcomeScreen() {
 
@@ -97,14 +155,23 @@ void login() {
 }
 
 int main() {
-
     welcomeScreen();
     login();
 
     Customer c1(1, "Janmesh", "Bhatt");
-    cout <<"\nCustomer: " << c1.getFirstName() <<""<< c1.getLastName()<< endl;
 
-    cout << "\n[Main menu will go here]" << endl;
+    SavingsAccount sa("SA001", &c1, 500.0, 2.5);
+    CheckingAccount ca("CA001", &c1, 300.0, 200.0);
+
+    sa.printReceipt();
+    sa.withdraw(450);   // should be denied - would go below $100
+    sa.withdraw(200);   // should work
+
+    cout << endl;
+
+    ca.printReceipt();
+    ca.withdraw(400);   // should work - within overdraft
+    ca.withdraw(200);   // should be denied - exceeds overdraft
 
     return 0;
 }
